4732852be2409084eabb7fafbaa23dfbe9946ebf
Bradley Anderson <13277940+bandrson@users.noreply.github.com>
2024-03-15 14:36:06 -0500

    Add Customer views

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -5,7 +5,7 @@ class Customer < ApplicationRecord
   validates :email_address, presence: true, format: { with: URI::MailTo::EMAIL_REGEXP }
   validates :notes, presence: true, allow_blank: true, length: { maximum: 10000 }
 
-    def self.ransackable_attributes(auth_object = nil)
+  def self.ransackable_attributes(auth_object = nil)
     ["created_at", "email_address", "full_name", "id", "id_value", "notes", "phone_number", "updated_at"]
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -8,5 +8,7 @@ Rails.application.routes.draw do
   get "up" => "rails/health#show", as: :rails_health_check
 
   # Defines the root path route ("/")
-  # root "posts#index"
+  root "customers#index", as: "customers"
+  get "/customers/alphabetized", to: "customers#alphabetized", as: "customers_alphabetized"
+  get "/customers/missing_email", to: "customers#missing_email", as: "customers_missing_email"
 end
