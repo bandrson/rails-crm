298c5aea65e3f5ebb5d269ae8e4aa3b3fc7afa20
Bradley Anderson <13277940+bandrson@users.noreply.github.com>
2024-03-15 00:38:34 -0500

    Add `image` attachment to the Customer model

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,2 +1,7 @@
 class Customer < ApplicationRecord
+  has_one_attached :image
+
+  def self.ransackable_attributes(auth_object = nil)
+    ["created_at", "email_address", "full_name", "id", "id_value", "notes", "phone_number", "updated_at"]
+  end
 end
