2212ffe0cf3bd95096f22305089a9b4381f8c346
Bradley Anderson <13277940+bandrson@users.noreply.github.com>
2024-03-15 00:47:55 -0500

    Add Customer model validations

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,7 +1,11 @@
 class Customer < ApplicationRecord
   has_one_attached :image
+  validates :full_name, presence: true, length: { minimum: 2 }
+  validates :phone_number, presence: true
+  validates :email_address, presence: true, format: { with: URI::MailTo::EMAIL_REGEXP }
+  validates :notes, presence: true, allow_blank: true, length: { maximum: 10000 }
 
-  def self.ransackable_attributes(auth_object = nil)
+    def self.ransackable_attributes(auth_object = nil)
     ["created_at", "email_address", "full_name", "id", "id_value", "notes", "phone_number", "updated_at"]
   end
 end
